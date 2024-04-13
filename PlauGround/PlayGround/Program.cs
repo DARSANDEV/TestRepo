d70e69f651e36eac3ab475dc51b66d1f95c074b1
DARSANDEV <ddarsandev@gmail.com>
2024-04-13 14:18:47 +0530

    Setup

---
+++ PlauGround/PlayGround/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+using PlayGround.Samples;
+
+Console.WriteLine("Welcome");
+
+bool retry = false;
+do
+{
+    Console.WriteLine("************* Game Starts *******************");
+    
+    var sampleObj = new SampleObject();
+    sampleObj.FindFactorial();
+
+    Console.WriteLine("--------------- Game completed -------------------");
+    Console.WriteLine(" Press 1 to try again ");
+    var  retryInput= int.Parse(Console.ReadLine());
+    retry = (retryInput ==1) ? true : false;
+} while (retry );
+
+
+ 
---
+++ PlauGround/PlayGround/Samples/Factorial.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlayGround.Samples
+{
+    public static class Factorial 
+    {
+        public static void FindFactorial(this SampleObject sample)
+        {
+            Console.WriteLine("Enter the Number");
+            int n = int.Parse(Console.ReadLine());
+            var output = Fact(n);
+            Console.WriteLine($"The factorial is {output}");
+        } 
+
+        private static int Fact(int num)
+        {
+            if (num ==0 || num == 1)
+            {
+                return 1;
+            }
+            else
+            {
+                return num * Fact(num - 1);
+            }
+        }
+    }
+}
